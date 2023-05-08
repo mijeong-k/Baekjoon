75a29d60e951de1da32561a26346372ecdb34670
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 16:58:30 +0900

    정올 2439번 문제

--- Baekjoon/src/q2439/Main.java
+++ Baekjoon/src/q2439/Main.java
@@ -9,17 +9,56 @@ public class Main {
 		int inp = sc.nextInt();
 		sc.close();
 
-		int num = 0;
+//		int num = 0;
+//		for (int i = 0; i < inp; i++) {
+//			for (int m = 1; m < inp - num; m++) {
+//				System.out.print(" ");
+//			}
+//			for (int n = 0; n <= num; n++) {
+//				System.out.print("*");
+//			}
+//			num++;
+//			System.out.println();
+//		}
+		
+		
+//		for (int i = 0; i < inp; i++) {
+//			for (int m = 0; m < inp -i -1; m++) {
+//				System.out.print(" ");
+//			}
+//			for (int n = 0; n <= i; n++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		for (int i = 0; i < inp; i++) {
+//			for (int m = 1; m < inp -i; m++) {
+//				System.out.print(" ");
+//			}
+//			for (int n = 0; n <= i; n++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
 		for (int i = 0; i < inp; i++) {
-			for (int i1 = 1; i1 < inp - num; i1++) {
-				System.out.printf(" ");
+			for (int j=inp-i-1; j>0; j--) {
+				System.out.print("?");
 			}
-			for (int i2 = 0; i2 <= num; i2++) {
+			for (int j = 0; j<1+i; j++) {
 				System.out.print("*");
 			}
-			System.out.print("\n");
-			num++;
+			System.out.println();
 		}
+
 	}
 
 }
+
+
+//		제일 바깥쪽 for문은 행
+//		그 다음 for문들은 열
+
+
+
