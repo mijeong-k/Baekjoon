a3c10cdc1dfed21604b35fc4e4fb77138606d2ec
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 16:16:44 +0900

    백준 2438번 문제

--- Baekjoon/src/q2438/Main.java
+++ Baekjoon/src/q2438/Main.java
@@ -9,14 +9,33 @@ public class Main {
 		int inp = sc.nextInt();
 		sc.close();
 
-		String str = "";
-		if (1 <= inp && inp <= 100) {
-			for (int i = 0; i < inp; i++) {
-				str = str + "*";
-				System.out.println(str);
-			}
 
+//		for (int i = 0; i < inp; i++) {
+//			for (int m = 0; m <= i; m++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		int num = 0;
+		for (int i = 0; i < inp; i++) {
+			for (int m = 0; m <= num; m++) {
+				System.out.print("*");
+			}
+			num++;
+			System.out.println();
 		}
+		
+		
+		
+//		String str = "";
+//		if (1 <= inp && inp <= 100) {
+//			for (int i = 0; i < inp; i++) {
+//				str = str + "*";
+//				System.out.println(str);
+//			}
+//
+//		}
 	}
 
 }
